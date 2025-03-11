1b5615ea908ae953b0f112f8ae8888e3f6b7137d
AVMatthews <abigail.v.matthews@gmail.com>
2025-03-11 19:49:06 -0400

    General functionality implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,110 @@
 import {useEffect, useRef, useState} from "react";
 import {io} from "socket.io-client";
 
+const ResponseType = {
+    INTIAL: "initialDocuments",
+    UPDATE: "updateDocuments",
+    BULK_UPDATE: "bulkUpdateDocuments"
+}
+
+//could make this take a port for connecting to different places
+//Can only subsription to a collection once per socket ... could add another ID
+class MongoConnection {
+    constructor() {
+        this.socket = io("http://localhost:5000");
+        this.requestCallbacks = {}; 
+        //one handler per subscription
+        // -> Could modify to accept handlers for different kinds of packets
+        //      (ie. initalDocuments, updateDocument, bufferedUpdateDocuments, Error, etc.)
+        this.subscriptionHandlers = {};
+        this.init();
+    }
+
+    init() {
+        //Modify to take multiple onUpdates which are called based on collection?
+        this.socket.on("initialDocuments", ({collectionName, data}) => {
+            this.subscriptionHandlers[collectionName]({
+                type: ResponseType.INTIAL,
+                results: data,
+            });
+        });
+
+        this.socket.on("updateDocuments", ({collectionName, change}) => {
+            this.subscriptionHandlers[collectionName]({
+                type: ResponseType.UPDATE,
+                results: change,
+            });
+        });
+
+        this.socket.on("bufferedUpdateDocuments", ({collectionName, changes}) => {
+            this.subscriptionHandlers[collectionName]({
+                type: ResponseType.BULK_UPDATE,
+                results: changes,
+            });
+        });
+
+        this.socket.on("findResults", ({ requestId, results }) => {
+            //console.log("request ID:" + requestId);
+            console.log(results);
+            const callback = this.requestCallbacks[requestId];
+            if (callback) {
+                callback(results);
+                delete this.requestCallbacks[requestId]; // Clean up
+            }
+        });
+
+        this.socket.on("findError", ({ requestId, message }) => {
+            console.log(requestId);
+            console.log(message);
+        });
+
+        this.socket.on("subscribeError", ({ collectionName, message }) => {
+            console.log(collectionName);
+            console.log(message);
+        });
+    }
+
+    subscribe(requestedCollection, pipeline, handler){
+        this.subscriptionHandlers[requestedCollection] = handler;
+        this.socket.emit("subscribe", {
+            collectionName: requestedCollection,
+            pipeline: pipeline,
+        });
+    }
+
+    //buffered subscribe
+    //server buffers packets until either change limit or emit delay is met
+    //change limit = number of changes
+    //emit delay = number of milliseconds until emit
+    bufferedSubscribe(requestedCollection, changeLimit, emitDelay, pipeline, handler){
+        this.subscriptionHandlers[requestedCollection] = handler;
+        this.socket.emit("bufferedSubscribe", {
+            collectionName: requestedCollection,
+            changeLimit: changeLimit,
+            emitDelay: emitDelay,
+            pipeline: pipeline,
+        });
+    }
+
+    find(collectionName, query, findOptions, callback){
+        const requestId = Date.now(); // Generate a unique request ID
+        //console.log(requestId);
+        this.requestCallbacks[requestId] = callback; // Store the callback
+        this.socket.emit("find", {
+            requestId: requestId,
+            collectionName: collectionName,
+            query: query,
+            options: findOptions,
+        });
+    }
+
+    //disconnect
+    endConnection(){
+        this.socket.disconnect("manual close");
+    }
+
+}
+
 const App = () => {
     const [documents, setDocuments] = useState([]);
     const hasInitedRef = useRef(false);
@@ -12,33 +116,101 @@ const App = () => {
         hasInitedRef.current = true;
 
         const collectionName = "result-cache-0";
+        const collectionName2 = "result-cache-1";
 
-        const socket = io("http://localhost:5000");
+        const doUpdate = (change) => {
+            if (change && change.operationType) {
+                setDocuments((prevDocs) => {
+                    if (change.operationType === "insert") {
+                        return [...prevDocs, change.fullDocument];
+                    } else if (change.operationType === "delete") {
+                        return prevDocs.filter(doc => doc._id !== change.documentKey._id);
+                    } else if (change.operationType === "update") {
+                        return prevDocs.map(doc => doc._id === change.documentKey._id
+                            ? { ...doc, ...change.updateDescription.updatedFields }
+                            : doc);
+                    } else {
+                        console.log("Unknown operation type:", change.operationType);
+                    }
+                    return prevDocs;
+                });
+            }  else {
+                console.error("Unexpected change structure:", change);
+            } 
+        };
 
-        socket.emit("getCollection", {
-            collectionName: collectionName,
-        });
-
-        socket.on("initialDocuments", ({data}) => {
-            setDocuments(data);
-        });
-        socket.on("updateDocuments", ({change}) => {
-            setDocuments((prevDocs) => {
-                if (change.operationType === "insert") {
-                    return [...prevDocs, change.fullDocument];
-                } else if (change.operationType === "delete") {
-                    return prevDocs.filter(doc => doc._id !== change.documentKey._id);
-                } else if (change.operationType === "update") {
-                    return prevDocs.map(doc => doc._id === change.documentKey._id
-                        ? {...doc, ...change.updateDescription.updatedFields}
-                        : doc);
-                } else {
-                    console.log(change);
+        const handleUpdate = ({ type, results }) => {
+            const change = results;
+            //console.log(type);
+            //console.log(results);
+            if (type === ResponseType.INTIAL) {
+                setDocuments(change);
+            } else if (type === ResponseType.UPDATE) {
+                doUpdate(change);
+            } else if (type === ResponseType.BULK_UPDATE) {
+                for (const c of change) {
+                    //console.log(c);
+                    doUpdate(c);
                 }
+            }
+            //Add else error case   
+        };
 
-                return prevDocs;
-            });
-        });
+        const handleUpdate2 = ({ type, results }) => {
+            console.log("Handling updates from second collection");
+        }
+
+        let mCollect = new MongoConnection();
+        const startOfDay = new Date();
+        startOfDay.setHours(0, 0, 0, 0); // Start of today
+        const startOfDayMillis = startOfDay.getTime(); // Get milliseconds
+
+        const endOfDay = new Date();
+        endOfDay.setHours(23, 59, 59, 999); // End of today
+        const endOfDayMillis = endOfDay.getTime(); // Get milliseconds
+
+        console.log("Start of Day (ms):", startOfDayMillis);
+        console.log("End of Day (ms):", endOfDayMillis);
+
+        //example pipeline
+        /* const pipeline = [
+            {
+                $match: {
+                    timestamp: {
+                        $gte: startOfDayMillis,
+                        $lte: endOfDayMillis
+                    }
+                }
+            }
+        ]; */
+        const pipeline = [];
+        
+        //example subscribe
+        mCollect.subscribe(collectionName, pipeline, handleUpdate);
+        console.log("subscribed")
+
+        //mCollect.subscribe(collectionName2, pipeline, handleUpdate2);
+        //console.log("subscribed to second collection")
+
+        //example buffered subscribe
+        //mCollect.bufferedSubscribe(collectionName, 2, 40000, pipeline, handleUpdate);
+        //console.log("buffered subscribe")
+
+        const handleResults = ({ results }) => {
+            console.log(results);
+            console.log("Results Handled!");
+        };
+
+        const query = {};
+        const projection = { _id: 0, timestamp: 1 };
+        const options = { limit: 2 };
+        const findOptions = { ...options, projection };
+        //example find
+        mCollect.find(collectionName, query, findOptions, handleResults);
+        console.log("Finish find!");
+
+        //example call to disconnect
+        //mCollect.endConnection();
     }, []);
 
     return (<>
--- server/main.mjs
+++ server/main.mjs
@@ -20,7 +20,6 @@ const io = new Server(app.server, {
     },
 });
 
-// Init replica set if not init'ed
 try {
     const mongoClient = new MongoClient("mongodb://localhost:27017",
         {replicaSet: "rs0", directConnection: true});
@@ -32,38 +31,214 @@ try {
 
 const client = new MongoClient("mongodb://localhost:27017");
 const collection = client.db("test").collection("result-cache-0");
+const collection2 = client.db("test").collection("result-cache-1");
 
 setInterval(() => {
-    collection.insertOne({timestamp: Date.now()}).then(() => {
-        app.log.debug("Inserted");
+    const doc = { timestamp: Date.now() };
+    collection.insertOne(doc).then(() => {
+        app.log.info("Inserted:", doc);
     }).catch(console.error);
-}, 1000);
+}, 20000);
 
-const watches = new Map();
+setInterval(() => {
+    const doc = { timestamp: Date.now() };
+    collection2.insertOne(doc).then(() => {
+        app.log.info("Inserted2:", doc);
+    }).catch(console.error);
+}, 20000);
 
-collection.watch().on("change", (change) => {
-    app.log.debug(change);
-});
+class MongoConnectionMgr {
+    constructor(client, dbName) {
+        this.client = client;
+        this.dbName =  dbName;
+        this.db = client.db(dbName);
+        this.collections = new Map;
+        //connectionID -> {collectionName: watcher}
+        this.watches = new Map();
+        //connectionID -> {collectionName: changeBackLog}
+        this.changeBuffers = new Map();
+        //connectionID -> {collectionName: limit}
+        this.changeLimits = new Map();
+        //connectionID -> {collectionName: timer}
+        this.changeTimers = new Map();
+        //connectionID -> {collectionName: emitDelay}
+        this.changeEmitDelays = new Map();
+        this.init();
+    }
 
-io.on("connection", (socket) => {
-    socket.on("getCollection", async ({collectionName}) => {
-        app.log.info(`Collection name ${collectionName} requested`);
-        if ("result-cache-0" !== collectionName) {
-            throw new Error(`Collection name ${collectionName} not currently supported`);
-        }
+    init() {
+        io.on("connection", (socket) => {
+            socket.on("subscribe", async ({collectionName, pipeline}) => {
+                this.subscribe(collectionName, pipeline, socket);
+            });
+
+            socket.on("bufferedSubscribe", async ({collectionName, changeLimit, emitDelay, pipeline}) => {
+                this.bufferedSubscribe(collectionName, changeLimit, emitDelay, pipeline, socket);
+            });
+
+            socket.on("find", async ({requestId, collectionName, query, options}) => {
+                app.log.info(requestId)
+                app.log.info(query);
+                app.log.info(options);
+                try{
+                    const findResults = await this.executeFind(collectionName, query, options);
+                    app.log.info(findResults);
+                    socket.emit("findResults", { requestId: requestId, results: findResults });
+                } catch (error){
+                    app.log.error(`Error finding documents in ${collectionName}: ${error.message}`);
+                    socket.emit("findError", { requestId, message: "Failed to find documents." });
+                }
+            });
+
+            socket.on("disconnect", (reason) => {
+                app.log.info(`Client disconnected: ${socket.id}`);
+                this.cleanup(socket.id);
+            });
+        });
+    }
+
+    //TODO: Add checks to both subscribe and buffered subscribe to check for duplicate subscribes
+    async subscribe(collectionName, pipeline, socket) {
+        let connectionID = socket.id;
+        if (false === this.collections.has(collectionName)) {
+            const databaseCollections = await this.db.listCollections().toArray();
+            let collectionExists = databaseCollections.some(collection => collection.name === collectionName);
+            if (false === collectionExists) {
+                socket.emit("subscribeError", { collectionName, message: `Collection name ${collectionName} does not exist in ${this.dbName}` });            
+            } else {
+                this.collections.set(collectionName, this.client.db(this.dbName).collection(collectionName));
+            }
+        }  
+        let requestedCollection = this.collections.get(collectionName);
 
         socket.emit("initialDocuments", {
-            data: await collection.find().toArray(),
+            collectionName: collectionName,
+            data: await requestedCollection.aggregate(pipeline).toArray(),
         });
-        if (false === watches.has(collectionName)) {
-            watches.set(collectionName, collection.watch());
+
+        if (false === this.watches.has(connectionID)) {
+            this.watches.set(connectionID, {});
+            app.log.info(`Watching first Collection: ${collectionName}`);
         }
-        watches.get(collectionName).on("change", (change) => {
+
+        //need to test that additional collections are handled correctly once the client can use the same socket for all connections
+        this.watches.get(connectionID)[collectionName] = requestedCollection.watch(pipeline);
+        app.log.info(`Watching Collection: ${collectionName}`);
+        
+        this.watches.get(connectionID)[collectionName].on("change", (change) => {
             socket.emit("updateDocuments", {
+                collectionName: collectionName,
                 change: change,
             });
         });
-    });
-});
+    }
+
+    async bufferedSubscribe(collectionName, changeLimit, emitDelay, pipeline, socket) {
+        let connectionID = socket.id;
+        app.log.info(`Collection name ${collectionName} requested`);
+        app.log.info(`ConnectionID ${connectionID}`);
+        if (false === this.collections.has(collectionName)) {
+            const databaseCollections = await this.db.listCollections().toArray();
+            let collectionExists = databaseCollections.some(collection => collection.name === collectionName);
+            if (false === collectionExists) {
+                socket.emit("subscribeError", { collectionName, message: `Collection name ${collectionName} does not exist in ${this.dbName}` });       
+            } else {
+                this.collections.set(collectionName, this.client.db(this.dbName).collection(collectionName));
+            }
+        }  
+        let requestedCollection = this.collections.get(collectionName);
+
+        socket.emit("initialDocuments", {
+            collectionName: collectionName,
+            data: await requestedCollection.aggregate(pipeline).toArray(),
+        });
+
+        if (false === this.watches.has(connectionID)) {
+            this.watches.set(connectionID, {});
+            this.changeBuffers.set(connectionID, {});
+            this.changeLimits.set(connectionID, {});
+            this.changeEmitDelays.set(connectionID, {});
+            this.changeTimers.set(connectionID, {});
+            app.log.info(`Buffered Watching first Collection: ${collectionName}`);
+        }
+        //needs to be tested once the client can use the same socket for all connections
+        //check if already subscribed to collection
+        this.watches.get(connectionID)[collectionName] = requestedCollection.watch(pipeline);
+        this.changeBuffers.get(connectionID)[collectionName] = [];
+        this.changeLimits.get(connectionID)[collectionName] = changeLimit;
+        this.changeEmitDelays.get(connectionID)[collectionName] = emitDelay;
+        this.changeTimers.get(connectionID)[collectionName] = null;
+        app.log.info(`Buffered Watching Collection: ${collectionName}`);
+    
+        this.watches.get(connectionID)[collectionName].on("change", (change) => {
+            // Buffer the change for bulk emission
+            this.bufferChangeForEmission(connectionID, collectionName, change, socket);
+        });
+    }
+
+    bufferChangeForEmission(connectionID, collectionName, change, socket) {
+        const buffer = this.changeBuffers.get(connectionID)[collectionName];
+        buffer.push(change);
+
+        // Clear any existing timer
+        if (this.changeTimers.get(connectionID)[collectionName]) {
+            clearTimeout(this.changeTimers.get(connectionID)[collectionName]);
+        }
+
+        // Set a new timer to emit the changes
+        this.changeTimers.get(connectionID)[collectionName] = setTimeout(() => {
+            this.emitBufferedChanges(connectionID, collectionName, socket);
+        }, this.changeEmitDelays.get(connectionID)[collectionName]);
+
+        const bufferLimit = this.changeLimits.get(connectionID)[collectionName];
+        const bufferLength = this.changeBuffers.get(connectionID)[collectionName].length;
+        if(bufferLength >= bufferLimit){
+            this.emitBufferedChanges(connectionID, collectionName, socket);
+        }
+    }
+
+    emitBufferedChanges(connectionID, collectionName, socket) {
+        const buffer = this.changeBuffers.get(connectionID)[collectionName];
+        if (buffer.length > 0) {
+            socket.emit("bufferedUpdateDocuments", {
+                collectionName: collectionName,
+                changes: buffer,
+            });
+            buffer.length = 0; // Clear the buffer
+        }
+    }
+
+    cleanup(connectionID) {
+        if (this.watches.has(connectionID)) {
+            for (let key in this.watches.get(connectionID)) {
+                const watcher = this.watches.get(connectionID)[key];
+                watcher.close(); 
+                delete this.watches.get(connectionID)[connectionID];
+                app.log.info(`Cleaned up resources for connection ID: ${connectionID}`);
+            }
+            this.watches.delete(connectionID);
+            app.log.info(`Removed ${connectionID} from watches map`);
+        }
+    }
+
+    async executeFind(collectionName, query, options) {
+        if (false === this.collections.has(collectionName)) {
+            const databaseCollections = await this.db.listCollections().toArray();
+            let collectionExists = databaseCollections.some(collection => collection.name === collectionName);
+            if (false === collectionExists) {
+                throw new Error(`Collection name ${collectionName} does not exist in ${this.dbName}`);            
+            } else {
+                this.collections.set(collectionName, this.client.db(this.dbName).collection(collectionName));
+            }
+        }    
+        const collection = this.collections.get(collectionName);
+        app.log.info(query);
+        app.log.info(options);
+        return await collection.find(query, options).toArray();
+    }
+
+}
+
+let mCollect = new MongoConnectionMgr(client, "test");
 
 await app.listen({port: 5000});
